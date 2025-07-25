b64d81ec3a529e3ea1f044eed754775531d73b05
Nguyen <nguyenduong251202@gmail.com>
2025-07-25 21:47:27 +1000

    Added background color using Container Wrapper along with LinearGradient object

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-  home: Scaffold(
-    body: Text('Sup Dawg'))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color(0xFF16171A),
+                Color(0xFF33FCFF),
+              ],
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'Sup Dawg',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
 }
