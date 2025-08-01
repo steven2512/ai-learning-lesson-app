8069141bdb454f973ebb7d67e3f658811cd91a30
Nguyen <nguyenduong251202@gmail.com>
2025-08-01 13:37:35 +1000

    Added obstacle and obstacle is moving from right to left

--- running_robot/lib/main.dart
+++ running_robot/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
@@ -17,20 +16,29 @@ void main() {
 
 class EmptyGame extends FlameGame with TapDetector {
   late Robot robot;
+  late Obstacle obstacle1;
 
   @override
   FutureOr<void> onLoad() async {
     //square object
-    robot = Robot(Vector2(size.x / 2 - 50, size.y / 2 - 50));
+    robot = Robot(
+      initialPosition: Vector2(size.x / 2 - 50, size.y / 2 - 50),
+    );
+
+    obstacle1 = Obstacle(
+      initialPosition: Vector2(size.x, size.y / 2),
+    );
     //change pos of object to middle of screen
 
-    //add Robot obj on screen
+    //add Objects to screen
     add(robot);
+    add(obstacle1);
   }
 
   @override
   void onTap() {
-    robot.velocity.y = -250;
+    robot.velocity.y = -400;
+    robot.isOnGround = false;
   }
 }
 
@@ -38,7 +46,11 @@ class Robot extends RectangleComponent {
   //Later on velocity changes when Flame calls update -> position of Robot changes
   Vector2 velocity = Vector2.zero();
   Vector2 initialPosition;
-  Robot(this.initialPosition)
+  final double gravity = 800;
+  bool isOnGround = true;
+
+  //Constructor
+  Robot({required this.initialPosition})
     : super(
         size: Vector2.all(100),
         paint: Paint()..color = Colors.blue,
@@ -50,12 +62,43 @@ class Robot extends RectangleComponent {
   @override
   void update(double dt) {
     super.update(dt);
+
+    //Add gravity if we accelerate upwards + falling down
+    if (velocity.y != 0) {
+      velocity.y += gravity * dt;
+    }
+    //standard force apply upward
     position += velocity * dt;
-    if (position.y <= initialPosition.y - 100) {
-      velocity.y = 250;
-    } else if (position.y > initialPosition.y && velocity.y > 0) {
-      position.y = initialPosition.y;
+
+    //Readjust position to the Ground when
+    //falling down past the initialPoint
+    if (position.y >= initialPosition.y) {
+      isOnGround = true;
       velocity.y = 0;
+      position.y = initialPosition.y;
+    }
+  }
+}
+
+class Obstacle extends RectangleComponent {
+  Vector2 initialPosition;
+  Vector2 velocity = Vector2(-200, 0);
+  bool isOnGround = true;
+
+  Obstacle({required this.initialPosition})
+    : super(
+        size: Vector2.all(50),
+        paint: Paint()..color = Colors.white,
+      ) {
+    position = initialPosition.clone();
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position.x += velocity.x * dt;
+    if (position.x <= -100) {
+      position.setFrom(initialPosition);
     }
   }
 }
